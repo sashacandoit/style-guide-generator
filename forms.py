c50facab0ce9987a329922a89ef33427e4ee61d4
Sasha Czerniawski <sashacandoit@gmail.com>
2022-07-07 06:58:42 -0400

    user forms added

---
+++ forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class AddUserForm(FlaskForm):
+    """Form for adding users."""
+
+    fullname = StringField('Full Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class UserUpdateForm(FlaskForm):
+    """Form for updating user profile."""
+
+    fullname = StringField('Full Name', validators=[DataRequired()])
+    username = StringField('Username')
+    email = StringField('E-mail', validators=[Email()])
+    image_url = StringField('(Optional) Image URL')
+    
+    password = PasswordField('Current Password')
