74f3d4ad958a10190297b75c4f63a76dbebdd6db
Sasha Czerniawski <sashacandoit@gmail.com>
2022-07-09 10:28:49 -0400

    register login and logout routes added and templates setup

--- project/app.py
+++ project/app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///capstone_1_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'somethingsecret'
 
 connect_db(app)
-db.create_all()
+# db.create_all()
 
 
 
@@ -26,6 +26,14 @@ db.create_all()
 #     print(item['family'], item['variants'])
 
 
+@app.route('/')
+def home_page():
+    """Render home page"""
+    
+    return render_template("home.html")
+
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register_user():
     form = AddUserForm()
@@ -36,12 +44,12 @@ def register_user():
         email = form.email.data
         password = form.password.data
         new_user = User.register(full_name, username, email,  password)
-        
-        flash('Welcome! Successfully created your account!')
 
         db.session.add(new_user)
         db.session.commit()
 
+        session['user_id'] = new_user.id
+        flash('Welcome! Successfully created your account!')
         return redirect('/')
         
     else:
@@ -49,10 +57,32 @@ def register_user():
 
 
 
-@app.route('/')
-def home_page():
-    """Render home page"""
 
-    return render_template("home.html")
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        
+        user = User.authenticate(username, password)
+        
+        #Check if use passes authentication:
+        if user:
+            flash(f"Welcome Back, {user.full_name}!", "primary")
+            session['user_id'] = user.id
+            return redirect('/')
+          
+        else:
+            form.username.errors = ['Invalid username/password.']
+
+    return render_template('login.html', form=form)
 
 
+
+@app.route('/logout')
+def logout_user():
+    session.pop('user_id')
+    flash("See you soon!", "info")
+    return redirect('/login')
